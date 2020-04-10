1edd298797e9831441877bd663e143491bb22321
David-Jianguang-Ran <jianguang.ran@gmail.com>
2020-04-10 12:48:29 -0600

    debugged entry point in ipython env

--- entry.py
+++ entry.py
@@ -6,8 +6,17 @@ from django.conf import settings
 from django.core.management import execute_from_command_line
 
 
+# set some global vars of my own (non-django for now)
+KMODEL_DIR = "records/kmodel"
+ARTIFACT_DIR = "records/artifact"
+
+# make data storage dir if one is not found
+os.makedirs("records",exist_ok=True)
+os.makedirs(KMODEL_DIR,exist_ok=True)
+os.makedirs(ARTIFACT_DIR,exist_ok=True)
+
 # configuring django
-BASE_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+BASE_DIR = os.getcwd()
 
 settings.configure(
     DATABASES={
@@ -25,9 +34,9 @@ django.setup()
 execute_from_command_line(["","makemigrations"])
 execute_from_command_line(["","migrate"])
 
-# set some global vars of my own (non-django for now)
-KMODEL_DIR = "records/kmodel"
-ARTIFACT_DIR = "records/artifact"
+
+
+from mod_man.interface import *
 
 if __name__ == "__main__" :
     execute_from_command_line(["","test"])
